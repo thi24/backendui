726e0aecf58a7a1f7c013b9cb443d36c4a261e3f
Klabautterr <htmlsite@skiff.com>
2024-03-24 17:47:03 +0100

    test

---
+++ Backend_Ui/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit="login">
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="username" required>
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" required>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login(event) {
+      event.preventDefault();
+
+      console.log('Username:', this.username);
+      console.log('Password:', this.password);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
