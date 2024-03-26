fbfa8b3592cb587580ff193f60dd104474f69a99
Moritz Reindl <moritz@Moritzs-MacBook-Pro.local>
2024-03-26 09:59:21 +0100

    First implementation of Login

--- Backend_Ui/pages/login.vue
+++ Backend_Ui/pages/login.vue
@@ -10,18 +10,13 @@ export default {
   methods: {
     async loginWithGitHub() {
       try {
-        // Starten Sie den Authentifizierungsfluss
         await this.$auth.loginWith('github');
 
-        // Warten Sie auf den Abschluss der Authentifizierung
         const authResult = await this.$auth.loginWith('github');
 
-        // Überprüfen Sie, ob die Authentifizierung erfolgreich war
         if (authResult.success) {
-          // Navigieren Sie zur Startseite oder einer anderen gewünschten Seite
           this.$router.push('/');
         } else {
-          // Behandeln Sie den Fehler oder zeigen Sie eine Fehlermeldung an
           console.error('Fehler bei der Authentifizierung:', authResult.error);
         }
       } catch (error) {
