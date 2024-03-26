6803904bb86904b3137b602e0588d09e9c89df9b
Moritz Reindl <moritz@Moritzs-MacBook-Pro.local>
2024-03-26 09:39:07 +0100

    First implemenation of login

--- Backend_Ui/nuxt.config.ts
+++ Backend_Ui/nuxt.config.ts
@@ -7,5 +7,17 @@ export default defineNuxtConfig({
 css: [
   "/assets/main.css",
 ],
-
+modules: [
+  '@nuxtjs/auth-next',
+  '@nuxtjs/axios',
+],
+auth: {
+  strategies: {
+    github: {
+      clientId: '149920e727612fa05202',
+      clientSecret: 'fd2cceff3d858b7265cdc39f7a4cf481e3c96e1c',
+      scope: ['user'],
+    },
+  },
+},
 })
